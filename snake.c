59c4865a4dce9e6e291d56ec57c32336ee9f67b1
Vibhayv2003 <vibhayv0@gmail.com>
2025-03-02 10:51:02 +0530

    Update snake.c

--- snake.c
+++ snake.c
@@ -16,4 +16,135 @@ enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
 enum eDirecton dir;
 
 void setup() {
-    gameOver = 
+    gameOver = 0;
+    dir = STOP;
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    appleX = rand() % WIDTH;
+    appleY = rand() % HEIGHT;
+    score = 0;
+}
+
+void draw() {
+    system("cls");
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (j == 0)
+                printf("#");
+            if (i == y && j == x)
+                printf("O");
+            else if (i == appleY && j == appleX)
+                printf("A");
+            else {
+                int print = 0;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        printf("o");
+                        print = 1;
+                    }
+                }
+                if (!print)
+                    printf(" ");
+            }
+
+            if (j == WIDTH - 1)
+                printf("#");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
+    printf("\n");
+    printf("Score: %d\n", score);
+}
+
+void input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            case 'x':
+                gameOver = 1;
+                break;
+        }
+    }
+}
+
+void logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    if (x >= WIDTH) x = 0;
+    else if (x < 0) x = WIDTH - 1;
+
+    if (y >= HEIGHT) y = 0;
+    else if (y < 0) y = HEIGHT - 1;
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = 1;
+    }
+
+    if (x == appleX && y == appleY) {
+        score += 10;
+        appleX = rand() % WIDTH;
+        appleY = rand() % HEIGHT;
+        nTail++;
+    }
+}
+
+int main() {
+    srand(time(0));
+    setup();
+    while (!gameOver) {
+        draw();
+        input();
+        logic();
+        Sleep(100); // Sleep to slow down the game loop
+    }
+    return 0;
+}
